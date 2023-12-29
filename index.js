174eca2968cdbc8c6f642779af885f348cbb1f2c
Zukhriddin <90769543+ZUKHRIDDINoff@users.noreply.github.com>
2023-12-30 00:51:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const bot = new Telegraf(config.get('TELEGRAM_BOT_TOKEN'));
 const Actions = require('./actions/index');
 const { start } = require('./main/start');
 
-bot.start(async ctx => start(ctx));
+bot.start(ctx => start(ctx));
 
 // Actions
-bot.action('checkAllFiles', async ctx => Actions.fileActions.checkAllFiles(ctx));
-bot.action('creatorMenu', async ctx => Actions.menuActions.creatorMenu(ctx, ctx.update.callback_query.from.id));
-bot.action('customerMenu', async ctx => Actions.menuActions.customerMenu(ctx));
-bot.action('checkSubscription', async (ctx) => Actions.subsFuncs.checkSubscription(ctx));
-bot.action('addNewFile', async ctx => Actions.fileActions.addNewFile(ctx));
+bot.action('checkAllFiles', ctx => Actions.fileActions.checkAllFiles(ctx));
+bot.action('creatorMenu', ctx => Actions.menuActions.creatorMenu(ctx, ctx.update.callback_query.from.id));
+bot.action('customerMenu', ctx => Actions.menuActions.customerMenu(ctx));
+bot.action('checkSubscription', (ctx) => Actions.subsFuncs.checkSubscription(ctx));
+bot.action('addNewFile', ctx => Actions.fileActions.addNewFile(ctx));
 
 
 // bot.on
-bot.on('document', async ctx => Actions.fileActions.documentFunctions(ctx))
+bot.on('document', ctx => Actions.fileActions.documentFunctions(ctx))
 
 // Callback functions
-bot.on('callback_query', async ctx => Actions.fileActions.callbackQueryFunc(ctx));
+bot.on('callback_query', ctx => Actions.fileActions.callbackQueryFunc(ctx));
 
 
 bot.launch();
